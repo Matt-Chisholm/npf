2b04fdeb15f142bd53667e76f162452d2bdd9df2
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-10-23 16:45:45 -0700

    nav bg

--- src/App.js
+++ src/App.js
@@ -5,35 +5,18 @@ import Home from "./screens/Home";
 import About from "./screens/About";
 import Contact from "./screens/Contact";
 import Projects from "./screens/Projects";
-import bg from "./images/vecteezy_abstract-gradient-pastel-blue-and-purple-background-neon_8617161.jpg";
 
 const App = () => {
-  const background = {
-    backgroundImage: `url(${bg})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    height: "100vh",
-    width: "100vw",
-    overflow: "hidden",
-    position: "fixed",
-    top: 0,
-    left: 0,
-    zIndex: -1,
-  };
-
   return (
-    <div style={background}>
-      <Router>
-        <Nav />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/projects' element={<Projects />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </Router>
   );
 };
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,23 +2,34 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Nav.css";
 import "../fonts/fonts.css";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faCode } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import bg from "../images/vecteezy_abstract-gradient-pastel-blue-and-purple-background-neon_8617161.jpg";
 
 const Nav = () => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const background = {
+    backgroundImage: `url(${bg})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  };
 
   const toggleMobileMenu = () => {
     setMobileMenuOpen(!isMobileMenuOpen);
   };
 
   return (
-    <nav className='navbar'>
+    <nav className='navbar' style={background}>
       <div className='nav-container'>
         <div className='logo'>
           <h1 style={{ fontFamily: "Tilt-Neon", marginLeft: "15%" }}>
             Matt Chisholm
           </h1>
+        </div>
+        <div className='logo'>
+          <FontAwesomeIcon icon={faCode} fade size='2xl' />
         </div>
         <div className='mobile-menu-toggle' onClick={toggleMobileMenu}>
           <FontAwesomeIcon icon={faBars} />
